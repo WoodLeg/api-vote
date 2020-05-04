e17dbb603a65aa59206320ca23a4652f19382660
Paul Souvestre <paul@qonto.eu>
2020-05-04 19:01:53 +0200

    Removed unecessary middleware

--- src/middlewares/index.js
+++
@@ -1,2 +0,0 @@
-export * from './authentification';
-export * from './identification';
--- src/router/ballots/ballots.router.js
+++ src/router/ballots/ballots.router.js
@@ -1,12 +1,11 @@
 import { Router } from 'express';
 import BallotController from './ballots.controller';
-import { isAuthenticated, identifyUser } from '../../middlewares';
-
+import { isAuthenticated } from 'middlewares/authentification';
 const router = Router();
 
 router.get('/', isAuthenticated, BallotController.getAll);
 router.get('/:ballotUrl', BallotController.getBallot);
-router.get('/:uuid/proceed', identifyUser, BallotController.proceedElection);
+router.get('/:uuid/proceed', isAuthenticated, BallotController.proceedElection);
 
 router.post('/', isAuthenticated, BallotController.createBallot);
 router.post('/:uuid/addVote', BallotController.addVote);
